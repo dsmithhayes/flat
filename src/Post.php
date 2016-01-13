6b077075b53d85c432d21255d9a6aa0429eab6a9
Dave Smith-Hayes <dsmithhayes@gmail.com>
2016-01-12 19:15:36 -0500

    Very basic.

--- src/Post.php
+++ src/Post.php
@@ -4,8 +4,32 @@ namespace Flat;
 
 class Post
 {
-    public function __construct(string $filePath)
-    {
+    /**
+     * @var string The full path in the filesystem of the markdown post
+     */
+    protected $path;
 
+    /**
+     * @var resource An open file handle
+     */
+    protected $handle;
+
+    /**
+     * @param string $path The full file path of the markdown file of the post
+     */
+    public function __construct(string $path)
+    {
+        if (!file_exists($path)) {
+            throw new \Exception("'" . $path . "' doesn't exists.");
+        $this->path = $path;
+        $this->handle = fopen($path, 'r+');
+    }
+
+    /**
+     * Enforce the handle to close when the object deconstructs.
+     */
+    public function __destruct()
+    {
+        fclose($this->handle);
     }
 }
