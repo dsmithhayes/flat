2183e066816f47a810faad563783f5ab75b10635
Dave Smith-Hayes <dsmithhayes@gmail.com>
2016-01-12 19:57:08 -0500

    Added some DI, added some documentation.

--- src/Post.php
+++ src/Post.php
@@ -2,24 +2,35 @@
 
 namespace Flat;
 
-use cebe\markdown\Markdown;
+use cebe\markdown\Parser;
 
 class Post
 {
     /**
-     * @var string The full path in the filesystem of the markdown post
+     * @var string
+     *      The full path in the filesystem of the markdown post
      */
     protected $path;
 
     /**
-     * @var resource An open file handle
+     * @var resource
+     *      An open file handle
      */
     private $handle;
 
     /**
-     * @param string $path The full file path of the markdown file of the post
+     * @var \cebe\markdown\Parser
+     *      An instance of the markdown parser
      */
-    public function __construct(string $path)
+    protected $parser;
+
+    /**
+     * @param string $path
+     *      The full file path of the markdown file of the post
+     * @param \cebe\markdown\Parser $parser
+     *      An instance of the cebe markdown Parser
+     */
+    public function __construct(string $path, Parser $parser)
     {
         if (!file_exists($path)) {
             throw new \Exception("'" . $path . "' doesn't exists.");
